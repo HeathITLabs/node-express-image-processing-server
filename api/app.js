bfb51c93131cc07706eb44c4dae0412602c52a95
jasonaheath <jason.heath@isclc.com>
2022-05-27 15:28:24 -0500

    Create the basic upload page

---
+++ api/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+const pathToIndex = path.resolve(__dirname, '../client/index.html')
+app.use("/*", (req, res) => {
+    res.sendFile(pathToIndex)
+}),
+
+module.exports = app;
